0f395955511dcb8510b3a93d7fa506188845b666
Jackihyun <pkhjack2325@gmail.com>
2025-04-03 23:57:47 +0900

    feat: 메인 페이지 및 헤더 컴포넌트 추가, 라우팅 구성 업데이트, 아이콘 svg파일 추가

---
+++ src/components/base/Header.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils/className";
+import MemoriaLogo from "@/assets/images/MemoriaLogo.svg";
+import BellIcon from "@/assets/images/BellIcon.svg";
+import ProflieIcon from "@/assets/images/ProfileIcon.svg";
+
+type Props = {
+  className?: string;
+};
+
+const Header: React.FC<Props> = ({ className }) => {
+  return (
+    <div className={cn("flex justify-between items-center h-20", className)}>
+      <img src={MemoriaLogo} alt="Memoria Logo" className={"w-24"} />
+      <div className="flex gap-6">
+        <img src={BellIcon} alt="Notification Bell" className={""} />
+        <img src={ProflieIcon} alt="Profile Icon" className={""} />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
