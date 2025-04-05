408508b4e7f8fe6b4ba60e9f2bbbe10e515181dd
Jackihyun <pkhjack2325@gmail.com>
2025-04-06 00:05:21 +0900

    feat: 헤더 컴포넌트에 로고 및 프로필 아이콘 클릭 시 라우팅 기능 추가

--- src/components/base/Header.tsx
+++ src/components/base/Header.tsx
@@ -2,18 +2,34 @@ import { cn } from "@/lib/utils/className";
 import MemoriaLogo from "@/assets/images/MemoriaLogo.svg";
 import BellIcon from "@/assets/images/BellIcon.svg";
 import ProflieIcon from "@/assets/images/ProfileIcon.svg";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   className?: string;
 };
 
 const Header: React.FC<Props> = ({ className }) => {
+  const navigate = useNavigate();
   return (
     <div className={cn("flex justify-between items-center h-10", className)}>
-      <img src={MemoriaLogo} alt="Memoria Logo" className={"w-24"} />
+      <img
+        src={MemoriaLogo}
+        alt="Memoria Logo"
+        className={"w-24"}
+        onClick={() => {
+          navigate("/main");
+        }}
+      />
       <div className="flex gap-6">
         <img src={BellIcon} alt="Notification Bell" className={""} />
-        <img src={ProflieIcon} alt="Profile Icon" className={""} />
+        <img
+          src={ProflieIcon}
+          alt="Profile Icon"
+          className={""}
+          onClick={() => {
+            navigate("/profile");
+          }}
+        />
       </div>
     </div>
   );
