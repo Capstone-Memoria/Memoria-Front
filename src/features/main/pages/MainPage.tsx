43b8dadd2b29c87a303cbddce53e8ce6feae4b7e
Jackihyun <pkhjack2325@gmail.com>
2025-04-07 00:31:10 +0900

    feat: 메인 페이지에 헤더 컴포넌트 추가 및 알림, 프로필 아이콘 구현

--- src/features/main/pages/MainPage.tsx
+++ src/features/main/pages/MainPage.tsx
@@ -1,4 +1,5 @@
 import Button from "@/components/base/Button";
+import Header from "@/components/base/Header";
 import PageContainer from "@/components/page/PageContainer";
 import { useAuthStore } from "@/stores/AuthenticationStore";
 import { useNavigate } from "react-router-dom";
@@ -14,7 +15,10 @@ const MainPage = () => {
 
   return (
     <PageContainer>
-      <div>안녕하세요, {authStore.context?.user?.nickName}님</div>
+      <Header />
+      <div className={"mt-5"}>
+        안녕하세요, {authStore.context?.user?.nickName}님
+      </div>
       <div className={"mt-4"}>
         <Button onClick={handleLogout}>로그아웃</Button>
       </div>
