b96d476ca3e6798b99bc8c627df3ea8eada520dd
천명현 <cmh1448@naver.com>
2025-04-10 10:30:56 +0900

    feat: ViewDiaryPage 헤더 구성 요소 추가 및 내비게이션 기능 구현

--- src/features/diary/pages/ViewDiaryPage.tsx
+++ src/features/diary/pages/ViewDiaryPage.tsx
@@ -1,9 +1,27 @@
 import Page from "@/components/page/Page";
+import { IoMdArrowBack, IoMdCalendar, IoMdMore } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
 const ViewDiaryPage = () => {
+  /* Properties */
+  const navigate = useNavigate();
+
   return (
     <Page.Container>
-      <Page.Header>Test Header</Page.Header>
+      <Page.Header>
+        <div className={"text-2xl"}>
+          <IoMdArrowBack onClick={() => navigate(-1)} />
+        </div>
+        <div>일기장 제목</div>
+        <div className={"flex text-2xl"}>
+          <div className={"p-2"}>
+            <IoMdCalendar />
+          </div>
+          <div className={"p-2"}>
+            <IoMdMore />
+          </div>
+        </div>
+      </Page.Header>
       <Page.Content>PageContent</Page.Content>
       <Page.Footer>Footer</Page.Footer>
     </Page.Container>
