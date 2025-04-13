4e9926a6bcf710a6cea5c389914dde13fc1388bf
천명현 <cmh1448@naver.com>
2025-04-14 00:57:45 +0900

    fix: DiaryCoverCarousel 에서 상하스크롤 방지 추가

--- src/components/diary/DiaryCoverCarousel.tsx
+++ src/components/diary/DiaryCoverCarousel.tsx
@@ -49,7 +49,7 @@ const DiaryCoverCarousel: React.FC<DiaryCoverCarouselProps> = ({
       <div
         ref={containerRef} // 컨테이너 ref 연결
         className={cn(
-          "relative w-full overflow-hidden cursor-grab",
+          "relative w-full overflow-hidden cursor-grab touch-pan-x",
           isDragging ? "cursor-grabbing" : "", // 드래그 중 커서 변경
           className
         )}
