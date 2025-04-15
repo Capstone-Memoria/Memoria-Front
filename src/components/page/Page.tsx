5d92b2159defdc6e6d64ef9f2c16eb7156f40b6e
천명현 <cmh1448@naver.com>
2025-04-16 00:30:09 +0900

    feat: Content 컴포넌트의 스타일 수정 및 불필요한 클래스 제거

--- src/components/page/Page.tsx
+++ src/components/page/Page.tsx
@@ -23,10 +23,7 @@ const Header: React.FC<HTMLAttributes<HTMLDivElement>> = ({ ...props }) => {
 
 const Content: React.FC<HTMLAttributes<HTMLDivElement>> = ({ ...props }) => {
   return (
-    <div
-      {...props}
-      className={cn("flex flex-col gap-5 px-4 py-4", props.className)}
-    >
+    <div {...props} className={cn("px-4 py-4", props.className)}>
       {props.children}
     </div>
   );
