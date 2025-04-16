7041407f21c362e21f0fc822c0a1eecf10c1c4d0
Jackihyun <pkhjack2325@gmail.com>
2025-04-16 20:12:18 +0900

    feat: 일기장 페이지에 오늘과 이전 글 구분 기능 추가 및 더미 데이터 수정

--- src/features/diary/pages/ViewDiaryPage.tsx
+++ src/features/diary/pages/ViewDiaryPage.tsx
@@ -23,10 +23,46 @@ const diaryList = [
     },
     summary:
       "오늘은 날씨가 좋아서 가고 싶었던 동네 카페에 나와서 카공을 좀 해봤다. 커피도 맛있고 날씨도 좋아서 콧노래가 절로 나오는 날이었다.",
-    createAt: DateTime.fromISO("2023-01-01"),
+    createAt: DateTime.fromISO("2025-04-16"),
     createdBy: {
       email: "pkhjack@naver.com",
       nickName: "기현",
+      createdAt: DateTime.fromISO("2023-01-01"),
+    },
+    likeCount: 2,
+    commentCount: 10,
+  },
+  {
+    title: "할리스에서 카공",
+    coverImage: {
+      id: "2",
+      fileName: "image.jpg",
+      size: 10,
+    },
+    summary:
+      "오늘은 날씨가 좋아서 가고 싶었던 동네 카페에 나와서 카공을 좀 해봤다. 커피도 맛있고 날씨도 좋아서 콧노래가 절로 나오는 날이었다.",
+    createAt: DateTime.fromISO("2025-04-16"),
+    createdBy: {
+      email: "pkhjack@naver.com",
+      nickName: "용석",
+      createdAt: DateTime.fromISO("2023-01-01"),
+    },
+    likeCount: 20,
+    commentCount: 2,
+  },
+  {
+    title: "느좋카페에서 카공",
+    coverImage: {
+      id: "1",
+      fileName: "image.jpg",
+      size: 10,
+    },
+    summary:
+      "오늘은 날씨가 좋아서 가고 싶었던 동네 카페에 나와서 카공을 좀 해봤다. 커피도 맛있고 날씨도 좋아서 콧노래가 절로 나오는 날이었다.",
+    createAt: DateTime.fromISO("2023-01-01"),
+    createdBy: {
+      email: "pkhjack@naver.com",
+      nickName: "진욱",
       createdAt: DateTime.fromISO("2023-01-01"),
     },
     likeCount: 2,
@@ -44,7 +80,7 @@ const diaryList = [
     createAt: DateTime.fromISO("2023-01-01"),
     createdBy: {
       email: "pkhjack@naver.com",
-      nickName: "기현",
+      nickName: "명현",
       createdAt: DateTime.fromISO("2023-01-01"),
     },
     likeCount: 2,
@@ -82,6 +118,17 @@ const ViewDiaryPage = () => {
       onClick: () => setIsPinned(!isPinned),
     },
   ];
+
+  // 날짜 적용
+  const today = DateTime.now().startOf("day");
+
+  const todayDiaries = diaryList.filter((diary) =>
+    diary.createAt.startOf("day").equals(today)
+  );
+
+  const previousDiaries = diaryList.filter(
+    (diary) => diary.createAt.startOf("day") < today
+  );
 
   return (
     <Page.Container>
@@ -147,17 +194,41 @@ const ViewDiaryPage = () => {
           placeholder={"작성자, 제목, 내용 검색"}
           className={"text-[13px] w-full mt-5"}
         />
+
         <div className={"flex flex-col gap-4 mt-5"}>
-          {isLoading
-            ? Array.from({ length: 6 }).map((_, index) => (
-                <div
-                  key={index}
-                  className={"animate-pulse bg-gray-200 h-32 rounded-sm"}
-                />
-              ))
-            : diaryList.map((diary, index) => (
-                <DiaryListItem key={index} item={diary} />
-              ))}
+          {isLoading ? (
+            Array.from({ length: 6 }).map((_, index) => (
+              <div
+                key={index}
+                className={"animate-pulse bg-gray-200 h-32 rounded-sm"}
+              />
+            ))
+          ) : (
+            <>
+              {todayDiaries.length > 0 && (
+                <>
+                  <div className={"flex justify-between items-center"}>
+                    <div className={"text-sm font-medium"}>오늘</div>
+                    <div className={"text-[11px] text-neutral-800"}>
+                      {today.toFormat("yyyy년 M월 d일")}
+                    </div>
+                  </div>
+                  {todayDiaries.map((diary, index) => (
+                    <DiaryListItem key={`today-${index}`} item={diary} />
+                  ))}
+                </>
+              )}
+
+              {previousDiaries.length > 0 && (
+                <>
+                  <div className={"text-sm font-medium"}>이전 글</div>
+                  {previousDiaries.map((diary, index) => (
+                    <DiaryListItem key={`prev-${index}`} item={diary} />
+                  ))}
+                </>
+              )}
+            </>
+          )}
         </div>
       </Page.Content>
     </Page.Container>
