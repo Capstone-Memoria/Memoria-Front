ea6fa47175cfcb014e46af86c14f050335c14ed9
천명현 <cmh1448@naver.com>
2025-05-20 22:53:48 +0900

    feat: LetterFromAI 컴포넌트에 스크롤 및 상태 관리 기능 추가
    - LetterFromAI 컴포넌트에 useResizeObserver 훅을 추가하여 스크롤 상태를 관리
    - 컴포넌트의 열림/닫힘 상태를 관리하기 위한 추가 상태 변수(isOverflowing, isScrolledOnTop) 도입
    - 스크롤이 가능한 경우 화살표 아이콘을 표시하여 사용자 경험 향상
    - Markdown을 사용하여 편지 내용을 표시하도록 수정

--- src/features/diary/components/ai/LetterFromAI.tsx
+++ src/features/diary/components/ai/LetterFromAI.tsx
@@ -1,9 +1,11 @@
 import Image from "@/components/base/Image";
 import { cn } from "@/lib/utils";
 import { AIComment } from "@/models/AIComment";
+import { useResizeObserver, useScroll } from "@reactuses/core";
 import { AnimatePresence, LayoutGroup, motion } from "motion/react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { GiEnvelope } from "react-icons/gi";
+import { MdArrowDownward } from "react-icons/md";
 import Markdown from "react-markdown";
 interface LetterFromAIProps {
   aiComment?: AIComment;
@@ -11,95 +13,129 @@ interface LetterFromAIProps {
 
 const LetterFromAI: React.FC<LetterFromAIProps> = ({ aiComment }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isOverflowing, setIsOverflowing] = useState(false);
+  const [isScrolledOnTop, setIsScrolledOnTop] = useState(false);
 
+  const containerRef = useRef<HTMLDivElement>(null);
+  useResizeObserver(containerRef, (entries) => {
+    const { height } = entries[0].contentRect;
+    const scrollHeight = containerRef.current?.scrollHeight;
+    if (!scrollHeight) {
+      setIsOverflowing(false);
+      return;
+    }
+
+    setIsOverflowing(scrollHeight > height);
+  });
+
+  const [_, y] = useScroll(containerRef);
   return (
     <LayoutGroup>
-      <motion.div
-        layout
-        transition={{
-          duration: 0.4,
-          ease: [0.16, 1, 0.3, 1],
-        }}
-        className={cn(
-          "border rounded-lg  min-h-12 relative max-h-60 overflow-y-scroll"
-        )}
-        onClick={() => setIsOpen(!isOpen)}
-      >
+      <motion.div layout className={"relative"}>
         <motion.div
           layout
-          key={"closed"}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          className={"flex items-center px-4 h-16 gap-4"}
+          transition={{
+            duration: 0.4,
+            ease: [0.16, 1, 0.3, 1],
+          }}
+          className={cn(
+            "border rounded-lg  min-h-12 relative max-h-60 overflow-y-scroll"
+          )}
+          ref={containerRef}
+          onClick={() => setIsOpen(!isOpen)}
         >
           <motion.div
             layout
-            className={
-              "size-9 rounded-full bg-gray-200 flex items-center justify-center overflow-hidden"
-            }
+            key={"closed"}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className={"flex items-center px-4 h-16 gap-4"}
           >
-            <Image
-              className={"size-full"}
-              imageId={aiComment?.createdBy.profileImage?.id}
-            />
-          </motion.div>
-          <AnimatePresence mode={"popLayout"}>
-            {!isOpen ? (
-              <motion.div
-                key={"closed"}
-                layout
-                initial={{ opacity: 0, filter: "blur(10px)" }}
-                animate={{ opacity: 1, filter: "blur(0px)" }}
-                exit={{ opacity: 0, filter: "blur(10px)" }}
-                className={"flex-1 flex items-center justify-between"}
-              >
-                <div>
+            <motion.div
+              layout
+              className={
+                "size-9 rounded-full bg-gray-200 flex items-center justify-center overflow-hidden"
+              }
+            >
+              <Image
+                className={"size-full"}
+                imageId={aiComment?.createdBy.profileImage?.id}
+              />
+            </motion.div>
+            <AnimatePresence mode={"popLayout"}>
+              {!isOpen ? (
+                <motion.div
+                  key={"closed"}
+                  layout
+                  initial={{ opacity: 0, filter: "blur(10px)" }}
+                  animate={{ opacity: 1, filter: "blur(0px)" }}
+                  exit={{ opacity: 0, filter: "blur(10px)" }}
+                  className={"flex-1 flex items-center justify-between"}
+                >
+                  <div>
+                    <motion.div className={"text-sm font-medium text-gray-900"}>
+                      {aiComment?.createdBy.name}으로부터 편지가 왔어요!
+                    </motion.div>
+                    <motion.div className={"text-xs text-gray-500"}>
+                      펼쳐서 확인하기
+                    </motion.div>
+                  </div>
+                  <div className={"text-blue-500 text-xl"}>
+                    <GiEnvelope />
+                  </div>
+                </motion.div>
+              ) : (
+                <motion.div
+                  key={"opened"}
+                  layout={"position"}
+                  initial={{ opacity: 0, filter: "blur(10px)" }}
+                  animate={{ opacity: 1, filter: "blur(0px)" }}
+                  exit={{ opacity: 0, filter: "blur(10px)" }}
+                  className={"flex-1 flex flex-col justify-between"}
+                >
                   <motion.div className={"text-sm font-medium text-gray-900"}>
-                    {aiComment?.createdBy.name}으로부터 편지가 왔어요!
+                    {aiComment?.title}
                   </motion.div>
                   <motion.div className={"text-xs text-gray-500"}>
-                    펼쳐서 확인하기
+                    {aiComment?.createdBy.name}
                   </motion.div>
-                </div>
-                <div className={"text-blue-500 text-xl"}>
-                  <GiEnvelope />
-                </div>
-              </motion.div>
-            ) : (
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </motion.div>
+          <AnimatePresence>
+            {isOpen && (
               <motion.div
-                key={"opened"}
-                layout={"position"}
-                initial={{ opacity: 0, filter: "blur(10px)" }}
-                animate={{ opacity: 1, filter: "blur(0px)" }}
-                exit={{ opacity: 0, filter: "blur(10px)" }}
-                className={"flex-1 flex flex-col justify-between"}
+                className={cn("flex-1 p-4 pt-0 min-h-32")}
+                initial={{ opacity: 0, y: -10, filter: "blur(10px)" }}
+                animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
+                exit={{ opacity: 0, y: -10, filter: "blur(10px)" }}
               >
-                <motion.div className={"text-sm font-medium text-gray-900"}>
-                  {aiComment?.title}
-                </motion.div>
-                <motion.div className={"text-xs text-gray-500"}>
-                  {aiComment?.createdBy.name}
-                </motion.div>
+                <div
+                  className={
+                    "text-sm font-medium text-gray-900 whitespace-pre-wrap"
+                  }
+                >
+                  <Markdown>{aiComment?.content}</Markdown>
+                </div>
               </motion.div>
             )}
           </AnimatePresence>
         </motion.div>
+
         <AnimatePresence>
-          {isOpen && (
+          {isOverflowing && y === 0 && isOpen && (
             <motion.div
-              className={cn("flex-1 p-4 pt-0 min-h-32")}
-              initial={{ opacity: 0, y: -10, filter: "blur(10px)" }}
-              animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
-              exit={{ opacity: 0, y: -10, filter: "blur(10px)" }}
+              className={
+                "absolute bottom-1 left-1/2 -translate-x-1/2 animate-bounce p-1 bg-white rounded-full border"
+              }
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              layout
             >
-              <div
-                className={
-                  "text-sm font-medium text-gray-900 whitespace-pre-wrap"
-                }
-              >
-                <Markdown>{aiComment?.content}</Markdown>
-              </div>
+              <MdArrowDownward className={"text-green-500"} />
             </motion.div>
           )}
         </AnimatePresence>
