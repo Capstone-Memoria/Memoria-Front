7a5eff00ef3cbe196b9422207a1aa4254b1e0d3b
yongseok <hpsyong@gmail.com>
2025-06-03 20:22:36 +0900

    fix: ImageSlider 컴포넌트의 클래스 이름을 문자열 템플릿으로 변경
    - JSX에서 클래스 이름을 문자열 템플릿으로 통일하여 가독성 향상

--- src/features/diary/components/ImageSlider.tsx
+++ src/features/diary/components/ImageSlider.tsx
@@ -57,10 +57,12 @@ const ImageSlider: React.FC<ImageSliderProps> = ({ images, onRemoveImage }) => {
   if (images.length === 0) return null;
 
   return (
-    <div className="relative w-full my-3 rounded-lg overflow-hidden bg-gray-100">
+    <div
+      className={"relative w-full my-3 rounded-lg overflow-hidden bg-gray-100"}
+    >
       <div
         ref={sliderRef}
-        className="w-full relative aspect-video"
+        className={"w-full relative aspect-video"}
         onTouchStart={handleTouchStart}
         onTouchMove={handleTouchMove}
         onTouchEnd={handleTouchEnd}
@@ -75,10 +77,12 @@ const ImageSlider: React.FC<ImageSliderProps> = ({ images, onRemoveImage }) => {
             <img
               src={image.url}
               alt={`이미지 ${index + 1}`}
-              className="w-full h-full object-contain"
+              className={"w-full h-full object-contain"}
             />
             <button
-              className="absolute top-2 right-2 bg-black/50 rounded-full p-1 text-white z-20"
+              className={
+                "absolute top-2 right-2 bg-black/50 rounded-full p-1 text-white z-20"
+              }
               onClick={(e) => {
                 e.stopPropagation();
                 onRemoveImage(index);
@@ -93,18 +97,26 @@ const ImageSlider: React.FC<ImageSliderProps> = ({ images, onRemoveImage }) => {
       {images.length > 1 && (
         <>
           <button
-            className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-black/30 text-white rounded-full p-1 z-20"
+            className={
+              "absolute left-2 top-1/2 transform -translate-y-1/2 bg-black/30 text-white rounded-full p-1 z-20"
+            }
             onClick={prevSlide}
           >
             <FiChevronLeft size={20} />
           </button>
           <button
-            className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-black/30 text-white rounded-full p-1 z-20"
+            className={
+              "absolute right-2 top-1/2 transform -translate-y-1/2 bg-black/30 text-white rounded-full p-1 z-20"
+            }
             onClick={nextSlide}
           >
             <FiChevronRight size={20} />
           </button>
-          <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 flex space-x-1 z-20">
+          <div
+            className={
+              "absolute bottom-2 left-1/2 transform -translate-x-1/2 flex space-x-1 z-20"
+            }
+          >
             {images.map((_, index) => (
               <div
                 key={index}
